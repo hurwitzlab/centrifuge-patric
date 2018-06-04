e540155ef11f474b462fc5dcf958da997e286135
Scott G. Daniel <scottdaniel25@gmail.com>
2018-06-04 18:46:56 -0500

    doing the full set now

--- stampede/test.sh
+++ stampede/test.sh
@@ -2,17 +2,17 @@
 
 #SBATCH -A iPlant-Collabs
 #SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 02:00:00
-#SBATCH -p development
+#SBATCH -n 68
+#SBATCH -t 24:00:00
+#SBATCH -p normal
 #SBATCH -J cntrfuge
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user scottdaniel@email.arizona.edu
 
-OUT_DIR="$WORK/centrifuge_test"
+OUT_DIR="$WORK/crc_mouse_centrifuged"
 
 export MY_PARAMRUN="$HOME/launcher/paramrun"
 
 [[ -d "$OUT_DIR" ]] && rm -rf $OUT_DIR/*
 
-bash run.sh -q "$WORK/in/dna" -f fastq -o $OUT_DIR -x 9606,32630
+bash run.sh -q "$WORK/in" -f fastq -o $OUT_DIR -x 9606,32630
