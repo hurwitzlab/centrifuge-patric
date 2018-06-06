3af9b57e9c5a6a032442d483f179e5b52ab8595e
Scott G. Daniel <scottdaniel25@gmail.com>
2018-06-06 10:53:08 -0500

    restart

--- stampede/test.sh
+++ stampede/test.sh
@@ -3,7 +3,7 @@
 #SBATCH -A iPlant-Collabs
 #SBATCH -N 1
 #SBATCH -n 68
-#SBATCH -t 24:00:00
+#SBATCH -t 48:00:00
 #SBATCH -p normal
 #SBATCH -J cntrfuge
 #SBATCH --mail-type BEGIN,END,FAIL
@@ -13,6 +13,6 @@ OUT_DIR="$WORK/crc_mouse_centrifuged"
 
 export MY_PARAMRUN="$HOME/launcher/paramrun"
 
-[[ -d "$OUT_DIR" ]] && rm -rf $OUT_DIR/*
+#[[ -d "$OUT_DIR" ]] && rm -rf $OUT_DIR/*
 
-bash run.sh -q "$WORK/in" -f fastq -o $OUT_DIR -x 9606,32630
+bash run.sh -k -q "$OUT_DIR/split" -f fastq -o $OUT_DIR -x 9606,32630
