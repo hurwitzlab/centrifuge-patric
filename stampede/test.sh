0c228f19f33807f5c887aa6fd4ae78bcca68d15b
Scott G. Daniel <scottdaniel25@gmail.com>
2018-06-14 17:24:33 -0500

    keep exisiting out_dir

--- stampede/test.sh
+++ stampede/test.sh
@@ -13,6 +13,6 @@ OUT_DIR="$WORK/crc_mouse_centrifuged"
 
 export MY_PARAMRUN="$HOME/launcher/paramrun"
 
-[[ -d "$OUT_DIR" ]] && rm -rf $OUT_DIR
+mkdir -p "$OUT_DIR"
 
 bash run.sh -q "$WORK/in" -f fastq -o $OUT_DIR -x 9606,32630 -k 
